d450353ddd7a46a7ba26e28489a19f4c1667acb0
nishat jahan <nishatjahanposhpa@gmail.com>
2026-02-05 12:32:56 +0600

    changed grid colunm style in dashboardHome page for cards

--- src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -250,7 +250,7 @@ const DashboardHome = () => {
       </div>
 
       {/* Stats Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-6">
         {stats.map((card, index) => (
           <motion.div
             key={index}
